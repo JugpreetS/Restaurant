f2627ce3cb666021a0185f16098a3b64e0bdbbb2
jugpreet singh <jugpreet89@gmail.com>
2016-01-04 00:24:58 +0530

    add ngresource to communicate with db

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,31 @@ app.controller('MenuController', ['$scope', 'menuFactory', function($scope, menu
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes=menuFactory.getDishes();
+    $scope.dishes={};
+    $scope.showMenu = false;
+    $scope.message = "Loading..."
+
+    // menuFactory.getDishes()
+    //     .then(
+    //         function(response){
+    //             $scope.dishes = response.data;
+    //             $scope.showMenu = true;
+    //         },
+    //         function(response){
+    //             $scope.message = "Error: "+response.status + " " + response.statusText;
+    //         }
+
+    //     );
+
+    menuFactory.getDishes().query(
+        function(response){
+            $scope.dishes = response;
+            $scope.showMenu = true;
+        },
+        function(response){
+            $scope.message = "Error: "+response.status+" "+response.statusText;
+        }
+    );
                 
     $scope.select = function(setTab) {
         $scope.tab = setTab;
@@ -47,7 +71,7 @@ app.controller('ContactController', ['$scope', function($scope) {
                 
 }]);
 
-app.controller('FeedbackController', ['$scope', function($scope) {
+app.controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
     
     $scope.sendFeedback = function() {
         
@@ -59,22 +83,50 @@ app.controller('FeedbackController', ['$scope', function($scope) {
         }
         else {
             $scope.invalidChannelSelection = false;
+
+            //save the feedback on the server.
+            //feedbackFactory.feedback().addFeedback($scope.feedback);
+
+
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
             $scope.feedback.mychannel="";
             $scope.feedbackForm.$setPristine();
-            console.log($scope.feedback);
         }
+
     };
 }]);
 
 app.controller('dishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-    var dish=menuFactory.getDish(parseInt($stateParams.id, 10));
-    $scope.dish = dish;
+    $scope.dish = {};
+    $scope.showDish = false;
+    $scope.message = "Loading...";
+    // menuFactory.getDish(parseInt($stateParams.id, 10))
+    //     .then(
+    //         function(response){
+    //             $scope.dish = response.data;
+    //             $scope.showDish = true;
+    //         },
+
+    //         function(response){
+    //             $scope.message = "Error: "+response.status+" "+response.statusText;
+    //         }
+    //     );
+
+    $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id, 10)})
+    .$promise.then(
+        function(response){
+            $scope.dish = response;
+            $scope.showDish = true;
+        },
+        function(response){
+            $scope.message = "Error: "+response.status+" "+response.statusText;
+        }
+    );
     
 }]);
 
-app.controller('DishCommentController', ['$scope', function($scope) {
+app.controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
     
     //Step 1: Create a JavaScript object to hold the comment from the form
     $scope.comment = {
@@ -91,6 +143,8 @@ app.controller('DishCommentController', ['$scope', function($scope) {
         
         // Step 3: Push your comment into the dish's comment array
         $scope.dish.comments.push($scope.comment);
+
+        menuFactory.getDishes().update({id : $scope.dish.id}, $scope.dish);
         
         //Step 4: reset your form to pristine
         $scope.dishCommentForm.$setPristine();
@@ -106,14 +160,72 @@ app.controller('DishCommentController', ['$scope', function($scope) {
 
 //implement the IndexController
 app.controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
-    $scope.featuredDish = menuFactory.getDish(0);
-    $scope.promotion = menuFactory.getPromotion(0);
+    $scope.featuredDish = {};
+    $scope.showDish = false;
+    $scope.message = "Loading...";
+    $scope.showPromotion = false;
+    $scope.promoMessage = "Loading..."
+    $scope.showLeadership = false;
+    $scope.leadershipMessage = "Loading...";
+    // menuFactory.getDish(0)
+    //     .then(
+    //         function(response){
+    //             $scope.featuredDish = response.data;
+    //             $scope.showDish = true;
+    //         },
+    //         function(response){
+    //             $scope.message = "Error: "+response.status+" "+response.statusText;
+    //         }
+    //     );
+
+    $scope.featuredDish = menuFactory.getDishes().get({id:0})
+        .$promise.then(
+            function(response){
+                $scope.featuredDish = response;
+                $scope.showDish = true;
+            },
+            function(response){
+                $scope.message = "Error: "+$scope.status+" "+$scope.statusText;
+           }
+        );
+
+    $scope.promotion = menuFactory.getPromotions().get({id : 0})
+        .$promise.then(
+            function(response){
+                $scope.promotion = response;
+                $scope.showPromotion = true;
+            },
+            function(response){
+                $scope.promoMessage = "Error: "+response.status+" "+response.statusText;
+            }
+        )
+
     //not hardcoding the Executive chef, instead passing the EC designation
-    $scope.execChef = corporateFactory.getLeader("EC");
-    console.log($scope.execChef);
+    //$scope.execChef = corporateFactory.getLeader("EC");
+    $scope.execChef = corporateFactory.getLeaders().get({id : 3})
+        .$promise.then(
+            function(response){
+                $scope.execChef = response;
+                $scope.showLeadership = true;
+            },
+            function(response){
+                $scope.leadershipMessage = "Error: "+response.status+" "+response.statusText;
+            }
+        )
 }]);
 
 //implement the AboutController
 app.controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory){
-    $scope.leaders = corporateFactory.getLeaders();
+    $scope.leadershipMessage = "Loading...";
+    $scope.showLeadership = false;
+
+    $scope.leaders = corporateFactory.getLeaders().query(
+        function(response){
+            $scope.leaders = response;
+            $scope.showLeadership =true;
+        },
+        function(response){
+            $scope.leadershipMessage = "Error: "+response.status+" "+response.statusText;
+        }
+    );
 }]);
